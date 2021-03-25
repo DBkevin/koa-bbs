e786f90c3d343442d73ce1e089fcda06fdfa9984
DBkevin <416606903@outlook.com>
2021-03-25 17:44:31 +0800

    数据填充完毕，列表首页完成

--- router/topics.js
+++ router/topics.js
@@ -1,3 +1,5 @@
+const db = require('../core/db');
+const timeago = require('timeago.js');
 exports = module.exports = {
     async index(ctx, next) {
         $topicsViewConfig = {
@@ -5,6 +7,12 @@ exports = module.exports = {
             pagename: '../topics/index',
             routerName: 'topics-index',
         }
+        let listSQL = `SELECT u.name AS U_name,u.avatar as U_avatar,u.id AS U_id,c.name AS C_name,c.id AS C_id,t.id AS T_id,t.title AS T_title,t.body AS T_body,t.updated_at,t.reply_count FROM topics t,users u,creategories c WHERE t.user_id=u.id AND t.crtegory_id=c.id limit 10`;
+        let topics = await db(listSQL);
+        topics.forEach(item=> {
+            item.updated_at = timeago.format(item.updated_at, 'zh_CN');
+        });
+        $topicsViewConfig.topics = topics;
         await ctx.render('layouts/index', $topicsViewConfig);
     },
 }
