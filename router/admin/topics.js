72a6265e3288e8a852f023fd9594d27a22b4206b
DBkevin <416606903@outlook.com>
2021-04-14 14:01:52 +0800

    后台编辑完成--后续待完善

--- router/admin/topics.js
+++ router/admin/topics.js
@@ -28,11 +28,34 @@ exports = module.exports = {
     },
     async edit(ctx, next) {
         let { id } = ctx.params;
-         let errors = {
-                name: '类名已经存在',
-                description: '类名已经存在--descript',
-            };
-            ctx.status = 409;
-            ctx.body = JSON.stringify(errors);
-    }
+        id = db.cape(id);
+        //检查是否存在改ID
+        let isNameSQL = `select * from categories where id=${id}`;
+        let isName = await db.query(isNameSQL);
+        if (isName) {
+            //接受参数。如果参数为空代表不更新
+            let { name, description } = db.escape(ctx.request.body, ['name', 'description']);
+            let updateCategorySQL = `update categories set  name=${name},description=${description} where id=${id};`;
+            let update = await db.query(updateCategorySQL);
+            if (update) {
+                let msg = {
+                    name: '更新成功'
+                };
+                ctx.body = JSON.stringify(msg);
+            } else {
+                let msg = {
+                    name:'error'
+                }
+                ctx.status = 409;
+                ctx.body = JSON.stringify(msg);
+            }
+        } else {
+            let msg = {
+                name: '该类不存在'
+            }
+            ctx.body = JSON.stringify(msg);
+       }
+    },
+    
+
 }
